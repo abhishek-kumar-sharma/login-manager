a45f5cbdc982957dd209acb8b77c0619475eef03
abhishek-kumar-sharma <mailtoabhisharma96@gmail.com>
2020-06-18 18:26:54 +0530

    Start for the day 18 June 2020

--- force-app/main/default/lwc/loginManagerLwc/loginManagerLwc.js
+++ force-app/main/default/lwc/loginManagerLwc/loginManagerLwc.js
@@ -3,14 +3,19 @@ import logo from '@salesforce/resourceUrl/logo';
 import validate_login_Apex from '@salesforce/apex/loginManagerCtrl.verifyUserLoginDetail_Apex';
 export default class LoginManagerLwc extends LightningElement {
 
-    showHeader = false; // to show hide the page header
-    showLogin = false; // to show hide the login box
-    showDataTable = false; // to show hide the data table
-    header_Logo = logo; // to hold the header logo
+    showHeader = false; // Variable to show hide the page header
+    showLogin = false; // Variable to show hide the login box
+    showDataTable = false; // Variable to show hide the data table
+    showLoginError = false; // Variable to show hide the login page error
+    loginErrorMessage ; // Variable to hold the login page
+    header_Logo = logo; // Variable to hold the header logo
+    @track currentTime = new Date();
     @track userDetails = {
         username: null, 
         password: null
     }; // to hold the user input details
+
+    @track userResponseFromServer; // Variable to hold the user response from server
  
     /**
      * Initalizing the values and checking the previous session
@@ -90,6 +95,14 @@ export default class LoginManagerLwc extends LightningElement {
                 validate_login_Apex({
                     userInput : JSON.stringify(this.userDetails)
                 }).then(result => { 
+                    this.userResponseFromServer = result;
+                    if(this.userResponseFromServer.isSuccess === false){
+                        this.showLoginError = true;
+                    }else{
+                        this.showLoginError = false;
+                        this.showLogin = false;
+                        this.showHeader = true;
+                    }
                     console.log('String ::',JSON.stringify(result));
                 })
                 .catch(error => {
@@ -101,6 +114,20 @@ export default class LoginManagerLwc extends LightningElement {
         } catch (error) {
             console.error('Exception occurred while handling the login button. Please reload the page and try again. \n Message ::', error.message);
 
+        }
+    }
+
+    /**
+     * Method to hide the error text from UI
+     * Created By       :       Abhishek Kumar Sharma
+     * Created Date     :       17 May 2020
+     */
+    handleCloseErrorButton(){
+        try {
+            this.showLoginError = false;
+        } catch (error) {
+            console.error('Error occurred while handling the close button click. Please refresh the page. \n Message ::',error);
+            
         }
     }
 
