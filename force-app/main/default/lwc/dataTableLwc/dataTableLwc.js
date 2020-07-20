dc18271f5640cd5877773668ac54684b92250bce
abhishek-kumar-sharma <mailtoabhisharma96@gmail.com>
2020-07-20 11:23:14 +0530

    Start For the day 20 July 2020

--- force-app/main/default/lwc/dataTableLwc/dataTableLwc.js
+++ force-app/main/default/lwc/dataTableLwc/dataTableLwc.js
@@ -16,20 +16,20 @@ export default class DataTableLwc extends LightningElement {
     defaultNoOfEntriesToShow = '5'; // No of entries to show the default
     totalNumberOfRows; // total no of rows
     totalPages; // total pages
+    totalRecords = 0; // total record in chuck
     sortedBy; // table sorted by
     sortedDirection; // table sorted direction
     @track entriesOptions = [
         { label: '5', value: '5', selected: true },
         { label: '10', value: '10' },
-        { label: '50', value: '50' },
-        { label: '100', value: '100' },
-        { label: '200', value: '200' },
-        { label: '500', value: '500' }
+        { label: '20', value: '20' },
+        { label: '50', value: '50' }
     ]; // entries select options values
     //Entries controlling attribute section end
 
     // Search input box controlling attribute section start
     searchString; // Attribute to hold the value for search input
+    isCalculating = false; // Attribute to hold the calculating status for search
     disableSearchBox = false; // Attribute to enable/disable the search box
     // Search input box controlling attribute section end
 
@@ -94,7 +94,6 @@ export default class DataTableLwc extends LightningElement {
     ]; // table columns
     dataTableObjectApiName = 'Login_Credential__c'; // object API name
     externalSpinner = false; // show external spinner except data table inline loading option
-    showSpinner = false; // data table is loading spinner
     showTableError = false; //Switch for handling the data table error template
     tableErrorMessage; // data table error message
     disableFirstButton = false; // Switch to control the enable/disable for first button
@@ -110,7 +109,19 @@ export default class DataTableLwc extends LightningElement {
     connectedCallback() {
         try {
             this.externalSpinner = true;
-            this.showSpinner = true;
+            this.getDataFromApex();
+        } catch (error) {
+            console.error('Error occurred while initalizing the data table. \n Message ::', error);
+        }
+    }
+
+    /**
+     * Method to handle the data refresh/fetch from server
+     * Created By       :       Abhishek Kumar Sharma
+    * Created Date      :       09 July 2020
+     */
+    getDataFromApex() {
+        try {
             getTableDataFrom_Apex({
                 objectApiName: this.dataTableObjectApiName,
                 viewAllPermissionForUser: this.viewAllPermissionForUser,
@@ -119,10 +130,10 @@ export default class DataTableLwc extends LightningElement {
                 .then(result => {
                     if (result.isSuccess === true) {
                         this.showTableError = false;
-                        this.showSpinner = false;
                         this.tableResponseData = result.responseData;
                         this.totalNumberOfRows = result.responseData.length;
                         this.tableDataFiltered = result.responseData;
+                        this.totalRecords = result.responseData.length
                         // Entries cal
                         this.fromEntries = 1;
                         if (this.toEntries < this.totalNumberOfRows) {
@@ -135,7 +146,6 @@ export default class DataTableLwc extends LightningElement {
                         // calling show record
                         this.showRecords();
                     } else {
-                        this.showSpinner = false;
                         this.showTableError = true;
                         this.disableEntriesOptions = true;
                         this.disableSearchBox = true;
@@ -146,7 +156,7 @@ export default class DataTableLwc extends LightningElement {
                     console.error('Error while getting data from apex in data table connected call back. \n Message ::', error);
                 })
         } catch (error) {
-            console.error('Error occurred while initalizing the data table. \n Message ::', error);
+            console.error('Exception while getting data from apex in data table connected call back. \n Message ::', error);
         }
     }
 
@@ -188,31 +198,18 @@ export default class DataTableLwc extends LightningElement {
      */
     handleShowEntriesChange(event) {
         try {
+            this.externalSpinner = true;
             if (event.target.value !== null && event.target.value !== undefined) {
                 this.defaultNoOfEntriesToShow = event.target.value;
-                this.externalSpinner = true;
                 this.showRecords();
             }
         } catch (error) {
+            this.externalSpinner = false;
             console.error('Error while handling the change entries. \n Message ::', error);
 
         }
     }
 
-    /**
-     * Method to handle the changes in the search box at top of the table
-     * Created By       :       Abhishek Kumar Sharma
-     * Created Date     :       24 June 2020
-     */
-    handleSearchStringChange() {
-        try {
-            if (event.target.value !== null && event.target.value !== undefined) {
-                this.searchString = event.target.value;
-            }
-        } catch (error) {
-            console.log('Error while handling the search string change. \n Message ::', error);
-        }
-    }
 
     /**
      * Method to handle the data refresh from server for data table
@@ -221,10 +218,10 @@ export default class DataTableLwc extends LightningElement {
      */
     handleRefreshTableContent() {
         try {
-
+            this.externalSpinner = true;
+            this.getDataFromApex();
         } catch (error) {
             console.error('Error occurred while handling the table refresh button. \n Message ::', error);
-
         }
     }
 
@@ -243,7 +240,7 @@ export default class DataTableLwc extends LightningElement {
             this.sortedDirection = event.detail.sortDirection;
             // calling sort Data In Asc Or Desc function to sort the data based on direction and selected field
             this.sortDataInAscOrDesc(event.detail.fieldName, event.detail.sortDirection);
-
+            this.externalSpinner = false;
         } catch (error) {
             console.error('Exception occurred while sorting the data. \n Message ::', error);
         }
@@ -274,12 +271,17 @@ export default class DataTableLwc extends LightningElement {
             });
             // set the sorted data to data table data
             this.tableDataFiltered = parseData;
-            this.externalSpinner = false;
+
         } catch (error) {
 
         }
     }
 
+    /**
+     * Method to move the data table at first page
+     * Created By       :       Abhishek Kumar Sharma
+     * Created Date     :       09 July 2020
+     */
     goToFirstPage() {
         try {
             this.currentPage = 1;
@@ -311,6 +313,12 @@ export default class DataTableLwc extends LightningElement {
             console.error('Error occurred while handling the page change to first. \n Message ::', error);
         }
     }
+
+    /**
+     * Method to handle the click of previous button of data table
+     * Created By       :       Abhishek Kumar sharma
+     * Created Date     :       09 July 2020
+     */
     goToPreviousPage() {
         try {
             this.currentPage = this.currentPage - 1;
@@ -335,15 +343,20 @@ export default class DataTableLwc extends LightningElement {
                 this.disableFirstButton = true;
                 this.disablePreviousButton = true;
                 return;
-            }else{
+            } else {
                 this.disableNextButton = false;
                 this.disableLastButton = false;
-
             }
         } catch (error) {
             console.error('Error occurred while handling the page change to previous. \n Message ::', error);
         }
     }
+
+    /**
+     * Method to handle the click on the next button
+     * Created By       :       Abhishek Kumar Sharma
+     * Created Date     :       09 July 2020
+     */
     goToNextPage() {
         try {
             this.currentPage = this.currentPage + 1;
@@ -368,7 +381,7 @@ export default class DataTableLwc extends LightningElement {
                 this.disableNextButton = true;
                 this.disableLastButton = true;
                 return;
-            }else{
+            } else {
                 this.disableFirstButton = false;
                 this.disablePreviousButton = false;
             }
@@ -376,6 +389,12 @@ export default class DataTableLwc extends LightningElement {
             console.error('Error occurred while handling the page change to next. \n Message ::', error);
         }
     }
+
+    /**
+     * Method to handle the click of last button 
+     * Created By       :       Abhishek Kumar Sharma
+     * Created Date     :       09 July 2020
+     */
     goToLastPage() {
         try {
             this.currentPage = this.totalPages;
@@ -408,4 +427,101 @@ export default class DataTableLwc extends LightningElement {
             console.error('Error occurred while handling the page change to last. \n Message ::', error);
         }
     }
+
+    /**
+     * Method to handle the changes in the search box at top of the table
+     * Created By       :       Abhishek Kumar Sharma
+     * Created Date     :       24 June 2020
+     */
+    handleSearchStringChange(event) {
+        try {
+            var searchString = event.target.value;
+            if (searchString !== null && searchString !== undefined && searchString.length > 2 && this.isCalculating === false) {
+                this.isCalculating = true;
+                var allRecords = this.tableResponseData;
+                var displayedFields = [];
+                /*
+                var displayColumns = c.get('v.tableColumns');
+                for (var i = 0; i < displayColumns.length; i++) {
+                    var displayColumn = displayColumns[i];
+                    displayedFields.push(displayColumn.fieldName);
+                }
+                */
+
+                displayedFields.push('Project_Name__c', 'Org_name__c', 'Username__c', 'Password__c', 'Security_Token__c'); // searching fields if all field needed uncomment the above loop
+                var filteredRecords = [];
+                for (var i = 0; i < allRecords.length; i++) {
+                    var allRecord = allRecords[i];
+                    var isFound = false;
+                    for (var key in allRecord) {
+                        if (allRecord.hasOwnProperty(key)) {
+                            if ((((allRecord[key]).toString()).toLowerCase()).indexOf(searchString.toLowerCase()) > -1 && displayedFields.indexOf(key) > -1) {
+                                isFound = true;
+                            }
+                        }
+                    }
+                    if (isFound) {
+                        filteredRecords.push(allRecord);
+                    }
+                }
+                var finalRecords = [];
+                for (var i = 1; i <= this.defaultNoOfEntriesToShow; i++) {
+                    if (i <= filteredRecords.length) {
+                        finalRecords.push(filteredRecords[i - 1]);
+                    } else {
+                        break;
+                    }
+                }
+
+                this.totalNumberOfRows = finalRecords.length;
+                // Entries cal
+                this.fromEntries = 1;
+                if (this.toEntries < this.totalNumberOfRows) {
+                    this.toEntries = this.defaultNoOfEntriesToShow;
+                } else {
+                    this.toEntries = this.totalNumberOfRows;
+                }
+
+                this.currentPage = 1;
+                if (finalRecords.length === 0) {
+                    this.fromEntries = 0;
+                    this.currentPage = 0;
+                } else {
+                    this.fromEntries = 1;
+                }
+                this.totalPages = Math.ceil(filteredRecords.length) / this.defaultNoOfEntriesToShow;
+                this.tableDataFiltered = finalRecords;
+                this.totalRecords = filteredRecords.length;
+                this.externalSpinner = false;
+
+                this.isCalculating = false;
+            } else if(this.isCalculating === false) {
+                this.isCalculating = true;
+                this.totalNumberOfRows = this.tableResponseData.length;
+                // Entries cal
+                this.fromEntries = 1;
+                if (this.toEntries < this.totalNumberOfRows) {
+                    this.toEntries = this.defaultNoOfEntriesToShow;
+                } else {
+                    this.toEntries = this.totalNumberOfRows;
+                }
+
+                this.currentPage = 1;
+                if (this.tableResponseData.length === 0) {
+                    this.fromEntries = 0;
+                    this.currentPage = 0;
+                } else {
+                    this.fromEntries = 1;
+                }
+                this.totalPages = Math.ceil(this.tableResponseData.length) / this.defaultNoOfEntriesToShow;
+                this.tableDataFiltered = this.tableResponseData;
+                this.totalRecords = this.tableResponseData.length;
+                this.externalSpinner = false;
+                this.isCalculating = false;
+            }
+        } catch (error) {
+            console.error('Error occurred while sorting the data on the input. \n Message ::', error);
+        }
+    }
+
 }
